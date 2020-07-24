fc1d9736d52432e2dd2554444894d51fb59d7de4
Cristian Henao <cristianhenao@ADKs-MacBook-Pro-CristianHenao.local>
2020-07-24 09:22:16 -0500

    cristianPAge

---
+++ precache-manifest.168a9e339b6ab3c78515ad0bd3720ac3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "540bea281cc6dc8861c42a00845d24e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43257a36d24b0fe03574",
+    "url": "/static/css/2.fd53ef4b.chunk.css"
+  },
+  {
+    "revision": "e9d13ad310233e9278a7",
+    "url": "/static/css/main.8453bda4.chunk.css"
+  },
+  {
+    "revision": "43257a36d24b0fe03574",
+    "url": "/static/js/2.809645f5.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/static/js/2.809645f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9d13ad310233e9278a7",
+    "url": "/static/js/main.81c61065.chunk.js"
+  },
+  {
+    "revision": "66a6d50c4b9291a8a0f0",
+    "url": "/static/js/runtime-main.b0fa0f04.js"
+  },
+  {
+    "revision": "39fe2236133975692172a5bbae55dac8",
+    "url": "/static/media/arrow.39fe2236.svg"
+  },
+  {
+    "revision": "c59da3268fcc19f5d97883b426d93ce0",
+    "url": "/static/media/bgBriefcase.c59da326.jpg"
+  },
+  {
+    "revision": "b592b4690e65f319f6422d4462c09c88",
+    "url": "/static/media/bgcontact.b592b469.png"
+  },
+  {
+    "revision": "33a6ed2b2f0524d18991e3679f83db16",
+    "url": "/static/media/bgintro.33a6ed2b.jpg"
+  },
+  {
+    "revision": "7b57f3f7ae9b26fd8282c84d42901337",
+    "url": "/static/media/bits.7b57f3f7.png"
+  },
+  {
+    "revision": "9bae59bfd65bb88bd8ad33a5f5779c9f",
+    "url": "/static/media/black-shape.9bae59bf.png"
+  },
+  {
+    "revision": "a8ee0b417a527123a0fb510caebcd62c",
+    "url": "/static/media/cristianweb.a8ee0b41.png"
+  },
+  {
+    "revision": "1a7d077548c5e6b96c9f53ed767048f1",
+    "url": "/static/media/email.1a7d0775.png"
+  },
+  {
+    "revision": "f9680a78336066cffcf16827115b58ec",
+    "url": "/static/media/instagram.f9680a78.png"
+  },
+  {
+    "revision": "eb38f05bdd3a141a7838d7db5060819d",
+    "url": "/static/media/logo.eb38f05b.svg"
+  },
+  {
+    "revision": "42aa6b85a7a945a8cd35eff049e32f82",
+    "url": "/static/media/quantronics.42aa6b85.png"
+  }
+]);
